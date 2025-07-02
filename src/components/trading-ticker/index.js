2f7b18ef1c8acb526f6eff28b75f8bea870df46f
Catur Pamungkas <c.pamungkas@oqtima.com>
2025-07-02 16:38:54 +0700

    fix: issue 1 - useContext TradingContext

--- src/components/trading-ticker/index.js
+++ src/components/trading-ticker/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import cn from "classnames";
 import PropTypes from "prop-types";
 import TradingSymbols from "./components/trading-symbols";
